ac62e596176c87d588752d0ed43978b8156d11e7
phamphuong140323 <phuongptph25249@fpt.edu.vn>
2022-06-14 01:17:35 +0700

    first commit

---
+++ js/index.js
@@ -0,0 +1,5 @@
+var button = document.getElementById("button");
+var menu = document.getElementById("menu");
+button.addEventListener("click", function () {
+    menu.classList.toggle("hidden");
+});
